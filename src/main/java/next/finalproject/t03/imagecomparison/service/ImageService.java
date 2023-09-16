49804cdb044c98359ca3633e09648641cbe7f503
VitorCav <vitorgomes22@gmail.com>
2023-09-16 16:32:06 -0300

    criacao do findAll() para buscar todas as imagens no banco e comparar

--- src/main/java/next/finalproject/t03/imagecomparison/service/ImageService.java
+++ src/main/java/next/finalproject/t03/imagecomparison/service/ImageService.java
@@ -3,7 +3,9 @@ package next.finalproject.t03.imagecomparison.service;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -61,13 +63,17 @@ public class ImageService {
         }
         return false;
     }
+
+    public List <ImageData> findAll(){
+        return repository.findAll();
+    }
     
-    private File convertMultiPartToFile(MultipartFile file ) throws IOException {
+    public File convertMultiPartToFile(MultipartFile file ) throws IOException {
         File convFile = new File( file.getOriginalFilename() );
         FileOutputStream fos = new FileOutputStream( convFile );
         fos.write( file.getBytes() );
         fos.close();
         return convFile;
-    }
+    }    
     
 } 
